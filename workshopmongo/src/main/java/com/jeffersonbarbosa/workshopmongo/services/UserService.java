bbbf9a993366819cfc8696daac7cbc5c692dab35
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-09-12 07:52:42 -0300

    feat:Obtendo um usuário por id

--- workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.jeffersonbarbosa.workshopmongo.services;
 
 import com.jeffersonbarbosa.workshopmongo.entities.User;
 import com.jeffersonbarbosa.workshopmongo.respositories.UserRepository;
+import com.jeffersonbarbosa.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Nenhum usuário encontrado."));
+    }
 
 }
