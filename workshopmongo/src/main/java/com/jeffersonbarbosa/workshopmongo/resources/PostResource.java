6d468ddf5bfac7503f982f11d881a61f6ae1a65a
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-09-18 08:32:13 -0300

    feat: consulta simples com query methods

--- workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/resources/PostResource.java
@@ -1,13 +1,11 @@
 package com.jeffersonbarbosa.workshopmongo.resources;
 
 import com.jeffersonbarbosa.workshopmongo.entities.Post;
+import com.jeffersonbarbosa.workshopmongo.resources.util.URL;
 import com.jeffersonbarbosa.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/services/PostService.java
+++ workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.jeffersonbarbosa.workshopmongo.services.exceptions.ObjectNotFoundExce
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +22,9 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Nenhum usuário encontrado."));
     }
 
+    public List<Post> findByTitle(String text){
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
 
 
 }
