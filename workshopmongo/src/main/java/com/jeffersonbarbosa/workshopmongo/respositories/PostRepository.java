57c5163d8614493ac1c49dedeb9a7c21958261bc
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-09-19 08:37:04 -0300

    feat: consulta simples com @query

--- workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/respositories/PostRepository.java
+++ workshopmongo/src/main/java/com/jeffersonbarbosa/workshopmongo/respositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.jeffersonbarbosa.workshopmongo.respositories;
 
 import com.jeffersonbarbosa.workshopmongo.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     //Querymethods
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    //Query simples
+    @Query(value = "{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
+
+
 
 }
